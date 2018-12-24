3a534c0c594995345ff647e7af23570f84cb0ebf
Paul Ignatiev <vo100193ipa@gmail.com>
2018-12-24 14:15:39 +0200

    Updates

---
+++ precache-manifest.2f9872fcee493459093bf8730ff40eac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b3293d637391543fb8e",
+    "url": "/react-publications/static/js/runtime~main.5b3293d6.js"
+  },
+  {
+    "revision": "c70565056d1436dac9ce",
+    "url": "/react-publications/static/js/main.c7056505.chunk.js"
+  },
+  {
+    "revision": "350119a621d87bba4bcf",
+    "url": "/react-publications/static/js/1.350119a6.chunk.js"
+  },
+  {
+    "revision": "c70565056d1436dac9ce",
+    "url": "/react-publications/static/css/main.8afb9e2c.chunk.css"
+  },
+  {
+    "revision": "7ea5ec659353e293a94bb4815a898477",
+    "url": "/react-publications/index.html"
+  }
+];
